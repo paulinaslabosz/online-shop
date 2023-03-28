277d406b097d18e49bac720c47520cdcf3cba32a
Paulina Słabosz <sejmipool@gmail.com>
2023-03-28 16:20:40 +0200

    Added form structure and styles

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import '../styles/Contact.css';
 
 function Contact() {
-  return <div>Kontakt</div>;
+  return (
+    <form className='form'>
+      <label className='form_header' htmlFor='textarea'>
+        {' '}
+        Skontaktuj się z nami
+      </label>
+      <textarea
+        className='form_textarea'
+        name='textarea'
+        id='textarea'
+        cols='30'
+        rows='10'
+      ></textarea>
+      <input className='form_button' type='submit' value='Wyślij' />
+    </form>
+  );
 }
 
 export default Contact;
