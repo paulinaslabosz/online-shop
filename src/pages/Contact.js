f6ac81ae881c538eab8dd01f8e245669bbf63e81
Paulina Słabosz <sejmipool@gmail.com>
2023-03-28 20:56:13 +0200

    Added contact form page and functions

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Contact.css';
 
 function Contact() {
+  const [formData, setFormData] = useState({
+    content: '',
+    active: false,
+  });
+
+  const handleSubmit = (e) => {
+    if (formData.content !== '') {
+      e.preventDefault();
+      setFormData({ ...formData, content: '', active: !formData.active });
+    } else {
+      alert('Błąd: pusty formularz');
+    }
+  };
+
   return (
-    <form className='form'>
+    <form className='form' onSubmit={handleSubmit}>
       <label className='form_header' htmlFor='textarea'>
         {' '}
         Skontaktuj się z nami
@@ -14,8 +28,13 @@ function Contact() {
         id='textarea'
         cols='30'
         rows='10'
+        value={formData.content}
+        onChange={(e) => setFormData({ ...formData, content: e.target.value })}
       ></textarea>
       <input className='form_button' type='submit' value='Wyślij' />
+      {formData.active ? (
+        <p className='form_comment'>Formularz został przesłany. Dziękujemy</p>
+      ) : null}
     </form>
   );
 }
