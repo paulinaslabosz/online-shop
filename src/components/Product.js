b45da263efd8593dab9d0e7394acefcdc09637b9
Paulina Słabosz <sejmipool@gmail.com>
2023-03-28 22:38:18 +0200

    Added full product component and styles

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import '../styles/Product.css';
 function Product(props) {
   return (
-    <div>
-      <h3>{props.title}</h3>
-      <p>{props.price} $</p>
+    <div className='product'>
+      <div className='img_wrapper'>
+        <img className='product_img' src={props.image} alt={props.name} />
+      </div>
+      <p className='product_price'>{props.price} $</p>
+      <h3 className='product_title'>{props.title}</h3>
     </div>
   );
 }
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Product from '../components/Product';
+import '../styles/Products.css';
+
 function Products() {
   const [products, setProducts] = useState([]);
   const productAPI = 'https://fakestoreapi.com/products';
@@ -11,9 +13,16 @@ function Products() {
   });
 
   const productsList = products.map((product) => (
-    <Product key={product.id} title={product.title} price={product.price} />
+    <Product
+      key={product.id}
+      title={product.title}
+      price={product.price}
+      image={product.image}
+      description={product.description}
+      category={product.category}
+    />
   ));
-  return <div>{productsList}</div>;
+  return <div className='products_wrapper'>{productsList}</div>;
 }
 
 export default Products;
