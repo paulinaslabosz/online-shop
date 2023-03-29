7efad78bb9ef6380bfe70c950ae446604ba6955b
Paulina Słabosz <sejmipool@gmail.com>
2023-03-29 15:05:41 +0200

    Added images in header section

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,7 +1,28 @@
 import React from 'react';
-
+import { Routes, Route } from 'react-router-dom';
+import '../styles/Header.css';
+import img1 from '../images/image1.jpg';
+import img2 from '../images/image2.jpg';
+import img3 from '../images/image3.jpg';
 function Header() {
-  return <div>Tu będzie obrazek</div>;
+  return (
+    <>
+      <Routes>
+        <Route
+          path='/'
+          element={<img className='image' src={img1} alt='girl' />}
+        ></Route>
+        <Route
+          path='/contact'
+          element={<img className='image' src={img2} alt='sth' />}
+        ></Route>
+        <Route
+          path='/products'
+          element={<img className='image' src={img3} alt='sthsth' />}
+        ></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default Header;
