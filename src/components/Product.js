b89bdec76078018669e02d8b0418447f3492c18a
Paulina Słabosz <sejmipool@gmail.com>
2023-04-25 21:56:43 +0200

    Added button 'Add To Cart' in component with list of products

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,15 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Product.css';
 function Product(props) {
+  const addToCart = () => {
+    console.log('Klik');
+  };
+
   return (
     <div className='product'>
       <div className='img_wrapper'>
         <img className='product_img' src={props.image} alt={props.name} />
       </div>
-      <p className='product_price'>{props.price} $</p>
       <Link to={`/productsList/${props.id}`} className='product_title'>
         {props.title}
       </Link>
+      <p className='product_price'>{props.price} $</p>
+
+      <button onClick={addToCart} className='product_addToCart'>
+        Add to cart
+      </button>
     </div>
   );
 }
