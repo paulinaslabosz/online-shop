c4526e49f0b1edaadc6530b21339e44ad98e362a
Paulina Słabosz <sejmipool@gmail.com>
2023-05-08 21:44:02 +0200

    Fixed: button is loaded after the poster is fully loaded

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Homepage.css';
 import img from '../images/SalePoster.png';
 function Homepage() {
+  const [isLoaded, setLoaded] = useState(false);
   return (
     <div className='homepage_poster'>
-      <img className='homepage_img' src={img} alt='Sale Poster' />
-      <Link className='homepage_button' to='/productsList'>
-        Shop now
-      </Link>
+      <img
+        onLoad={() => setLoaded(true)}
+        className='homepage_img'
+        src={img}
+        alt='Sale Poster'
+      />
+      {isLoaded ? (
+        <Link className='homepage_button' to='/productsList'>
+          Shop now
+        </Link>
+      ) : null}
     </div>
   );
 }
